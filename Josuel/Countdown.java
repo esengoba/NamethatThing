81cfdaff0589b239ea6f7b7ae03571bde56b782c
Josuel M <Joshlix95@users.noreply.github.com>
2016-10-25 23:55:15 -0400

    Countdown made great

---
+++ Josuel/Countdown.java
@@ -0,0 +1,29 @@
+/**
+         * Implement the countdown function
+         * This is called when the the user clicks Ready
+         */
+        private void countdown(){
+            // Create a text counter
+            JLabel counter = new JLabel("Counter from 10 to 0");
+            counter.setFont(Constants.QUESTION_FONT);
+            add(counter);
+
+            final Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                int i = Integer.parseInt(Constants.COUNTMAX);
+                public void run() {
+                    int temp = i--;
+                    counter.setText(String.valueOf(temp));
+
+                    if (temp >= 7)
+                        counter.setForeground(Color.GREEN);
+                    else if (temp > 3)
+                        counter.setForeground(Color.YELLOW);
+                    else
+                        counter.setForeground(Color.RED);
+
+                    if (i< 0)
+                        timer.cancel();
+                }
+            }, 0, 1000);
+        }
